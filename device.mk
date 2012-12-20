ce14472f163eb4e14a1009d8095bde68b516eda5
Tom Marshall <tdm.code@gmail.com>
2012-12-20 00:07:50 -0800

    Bluetooth fixes.
    * Make init.qcom.bt.sh device specific.
    * Add Bluetooth firmware to model files for multi-rom build.

--- device.mk
+++ device.mk
@@ -6,6 +6,10 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/i_atnt/overlay
 
 # XXX: some (most?) of these belong in iproj/device.mk
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+	device/lge/i_atnt/prebuilt/init.qcom.bt.sh:system/bin/init.qcom.bt.sh
+
 PRODUCT_COPY_FILES += \
 	device/lge/i_atnt/prebuilt/root/init.iproj.rc:root/init.iproj.rc \
 	device/lge/i_atnt/prebuilt/root/init.iproj.usb.rc:root/init.iproj.usb.rc \
